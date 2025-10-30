c1888853ebe2affb6a8f6392d860bfd819a19615
Adherly Cisneros <acisnerosx@gmail.com>
2025-10-30 00:06:39 -0500

    fixed restart button to match theme button styles

--- src/components/rooms/Room3Displacers.jsx
+++ src/components/rooms/Room3Displacers.jsx
@@ -18,7 +18,7 @@ export default function Room3Displacers({
   darklordDead,
   chxospixieDead,
 }) {
-  const [enemyHealth, setEnemyHealth] = useState(1); //180 health TEST
+  const [enemyHealth, setEnemyHealth] = useState(180); //180 health TEST
   const [enemyDefeated, setEnemyDefeated] = useState(false);
   const [showRedFlash, setShowRedFlash] = useState(false);
   const [enemyPose, setEnemyPose] = useState("idle");
@@ -113,7 +113,7 @@ export default function Room3Displacers({
 
     if (!target) return;
 
-    const damage = Math.floor(Math.random() * 6) + 10; // TEST
+    const damage = 200;//Math.floor(Math.random() * 6) + 10; // TEST
 
     if (target === "Darklord") {
       setDarklordHealth((prev) => Math.max(prev - damage, 0));
